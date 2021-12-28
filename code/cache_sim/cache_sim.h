97a893331e4e544335898dff19831bb1339bc165
defychen <894739534@qq.com>
2021-12-28 08:26:26 +0800

    Reading notes 2021/12/28

--- code/cache_sim/cache_sim.h
+++ code/cache_sim/cache_sim.h
@@ -11,9 +11,10 @@
 #include <cmath>
 #include <ctime>
 #include <string>
+#include <vector>
 using namespace std;
 
-#define MAX_CACHELINE_NUM (65536)
+#define MAX_CACHELINE_NUM (65536)	/* 65536(2^16) */
 //#define MAX_CACHELINE_NUM (268435456) /* The max num of gcc array support 268435456(2^28) */
 
 /* 内存地址与Cache地址的关联方式: 直接映射、组相联、全相联 */
@@ -24,19 +25,90 @@ enum associativity_type {
 };
 
 /* 替换策略: NONE(直接替换)、FIFO(先进先出算法)、LRU(最近最少使用算法)、
-	LRU(最经常使用替换算法)、Random(随机替换算法) */
+	LRU(最不经常使用替换算法)、Random(随机替换算法) */
 enum replace_type {
 	NONE = 0,
 	FIFO = 1,
 	LRU = 2,
 	LFU = 3,
-	Random = 4
+	Random = 4,
+	PLRU = 5
 };
 
-/* 写替换: write_through、write_back */
+/* 写策略: write_through(全写法)、write_back(回写法) */
 enum write_type {
 	WRITE_THROUGH = 1,
 	WRITE_BACK = 2
+};
+
+class plru_replace {
+public:
+	vector<bool> plru_bit_vector;
+public:
+	plru_replace() {}
+	~plru_replace() {}
+	void reset_plru()
+	{
+		for (uint32_t i = 0; i < plru_bit_vector.size(); i++) {
+			plru_bit_vector.at(i) = false;
+		}
+	}
+
+	uint32_t flip_plru()
+	{
+		uint32_t i = 0;
+		uint32_t j = 0; /* j represent the number of binary tree layer */
+		while ((i * 2 + 1) < plru_bit_vector.size()) {
+			plru_bit_vector.at(i) = !plru_bit_vector.at(i);
+			j += 1;
+			if (plru_bit_vector.at(i)) { /* indicate it's left subtree if plru bits is true after flip plru */
+				i = i * 2 + 1;
+			} else { /* indicate it's right subtree if plru bits is false after flip plru */
+				i = i * 2 + 2;
+			}
+		}
+		plru_bit_vector.at(i) = !plru_bit_vector.at(i);
+		j += 1;
+
+		if (plru_bit_vector.at(i)) { /* left subtree */
+			i = i * 2 + 1;
+		} else { /* right subtree */
+			i = i * 2 + 2;
+		}
+		return (i - ((j << 1) - 1));
+	}
+
+	void reverse_flip_plru(uint32_t index)
+	{
+		uint32_t n = floor(log2(plru_bit_vector.size() + 1)) - 1; /* n represent the number of binary tree layer */
+		uint32_t i = 0; /* node number */
+
+		i = (1 << n) - 1 + (index / 2); /* the leaf node */
+		n -= 1;
+		if (index % 2 == 0) { /* left subtree */
+			plru_bit_vector.at(i) = true;
+		} else { /* right subtree */
+			plru_bit_vector.at(i) = false;
+		}
+
+		index = i;
+		while (n != 0) {
+			i = (index - 1) / 2;
+			if (index % 2 == 0) { /* right subtree */
+				plru_bit_vector.at(i) = false;
+			} else { /* left subtree */
+				plru_bit_vector.at(i) = true;
+			}
+			n -= 1;
+			index = i;
+		}
+
+		if (index == 1) {
+			plru_bit_vector.at(0) = true;
+		} else {
+			plru_bit_vector.at(0) = false;
+		}
+	}
 };
 
 class cache_sim {
